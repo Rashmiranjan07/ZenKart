f22fab29986b77433b496d9937a37840f97cbcaa
Rashmiranjan shaw <heyyrashmiranjan02@gmail.com>
2026-04-21 23:40:30 +0530

    Zenkart

---
+++ src/main/java/com/product/configuration/SpringSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.product.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+	@Bean
+	public SecurityFilterChain configureFilterChain(HttpSecurity http) throws Exception {
+		http.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+				.formLogin(form -> form.disable()).httpBasic(basic -> basic.disable());
+		return http.build();
+	}
+
+	@Bean
+	public PasswordEncoder creatPasswordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
